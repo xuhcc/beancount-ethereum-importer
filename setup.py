7ff0a5cfb95e364039eb63bd5d14ab8c4f1dbb5e
Kirill Goncharov <kdgoncharov@gmail.com>
2020-11-11 00:21:34 +0300

    Bump version: 1.0.0 → 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='beancount-ethereum',
-    version='1.0.0',
+    version='1.1.0',
     description='Ethereum transaction importer for Beancount',
     packages=['beancount_ethereum'],
     license='GPLv3',
