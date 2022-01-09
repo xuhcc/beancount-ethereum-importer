60d6cf1c32e100f708d6e2986df9bb857ee69ec9
xuhcc <xuhcc@cryptolab.net>
2022-01-09 20:58:29 +0300

    Bump version: 1.4.1 → 1.4.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='beancount-ethereum',
-    version='1.4.1',
+    version='1.4.2',
     description='Ethereum transaction importer for Beancount',
     packages=['beancount_ethereum'],
     license='GPLv3',
